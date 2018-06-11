e745d505aeae1771be023728c5db977f0d5d6935
adam stogner <stognera@gmail.com>
2018-06-10 20:27:16 -0400

    adding stg

--- main.tf
+++ main.tf
@@ -4,60 +4,6 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-40d28157"
-  instance_type = "t2.micro"
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-
-  tags {
-    Name = "terraform-example"
-  }
-}
-
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "adam-terraform-up-and-running-state-v2"
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_statelock" {
-  name           = "terraform_statelock"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
 terraform {
   backend "s3" {
     bucket         = "adam-terraform-up-and-running-state-v2"
---
+++ stg/services/webserver-cluster/var.tf
@@ -0,0 +1,14 @@
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default     = 8080
+}
+
+variable "db_remote_state_bucket" {
+  description = "The name of the S3 bucket used for the database's remote state storage"
+  default     = "adam-terraform-up-and-running-state-v2"
+}
+
+variable "db_remote_state_key" {
+  description = "The name of the key in the S3 bucket used for the database's remote state storage"
+  default     = "global/webserver-cluster/terraform.tfstate"
+}
